199e5349a3cf1defe3217fa85b33c639656019f9
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2023-10-23 10:12:53 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = "59"
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber)
+
+//------------------------------------------//
+// let score1 = "59sdf"
+// let valueInNumber1 = Number (score1)
+// console.log(typeof valueInNumber1) // Number
+// console.log(valueInNumber1) // NaN
+
+//------------------------------------------//
+// let score3 = null
+// let valueInNumber3 = Number (score3)
+// console.log(typeof valueInNumber3) // number
+// console.log(valueInNumber3) // 0
+
+// "65" => 65
+// "23mf" => NaN
+// true/false => 1/0
+
+//-------------------------------------------//
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Sachin" => true
+
+//------------------------------------------//
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
