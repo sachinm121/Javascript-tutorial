a6c7efbc00e96b065eefe641f7be18b0cd35cf3e
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2023-10-25 08:55:17 +0000

    function with object

--- 03basics/01_functions.js
+++ 03basics/01_functions.js
@@ -65,6 +65,5 @@ const myNewArray = [200, 400, 100, 600]
 function returnSecondValue(getArray){
     return getArray[1]
 }
-
 // console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
