44d52b787da5b959a16f53399d47a48476d8802d
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2023-10-25 07:18:11 +0000

    object done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,6 +26,7 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = { obj1, obj2 }
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
+// spread
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
