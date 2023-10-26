e74235fde318ccda0fab74ccb8f0685b60a6e864
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2023-10-26 05:54:43 +0000

    for in loop with objects

---
+++ 05_iterations/04_forIn_object.js
@@ -0,0 +1,30 @@
+// Differnce between for...of and for...in loop
+// https://medium.com/@SamXprt/difference-between-for-of-and-for-in-loop-in-javascript-98f5afbcc57b
+// https://g.co/bard/share/5877d1cde4dd
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
