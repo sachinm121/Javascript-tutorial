315c9b2427b70d10bade4f5a46553cb810614ec6
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2023-11-03 21:51:27 +0000

    bmi completed

--- 07_projects/02_bmi_cal/script.js
+++ 07_projects/02_bmi_cal/script.js
@@ -1,1 +1,22 @@
-const input1 = document.querySelector()
+const form = document.querySelector("form")
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    let height = parseInt(document.querySelector("#height").value)
+    let weight = parseInt(document.querySelector("#weight").value)
+    console.log(height, weight)
+
+    let res = document.querySelector("#result")
+    
+    if(height === '' || height < 0 || isNaN(height)){
+        res.innerHTML = `Please enter valid height ${height}`
+    }
+    if(weight === '' || weight < 0 || isNaN(weight)){
+        res.innerHTML = `Please enter valid weight ${weight}`
+    }
+    else{
+        const bmi = (weight /( (height*height)/10000)).toFixed(2)
+        res.innerHTML = `<span>${bmi}</span>`
+    }
+})
