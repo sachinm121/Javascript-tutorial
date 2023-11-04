46bbe0475ce1de8b453be9ceca0b4fa29605782b
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2023-11-04 01:55:46 +0000

    guess the number

---
+++ 07_projects/04_gusse_the_number/script.js
@@ -0,0 +1,9 @@
+const randomNumber = parseInt(Math.random() * 100 + 1)
+
+const submit = document.querySelector("#subt")
+const userInput = document.querySelector("#inputField")
+const guessSlot = document.querySelector("#guesses")
+const lastResult = document.querySelector("#lastResult")
+const lowOrHi = document.querySelector("#lowOrHi")
+
+
