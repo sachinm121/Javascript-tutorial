85c007f6e686219dfed47efe5e974dd4692ce835
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2023-11-04 04:51:24 +0000

    updated

--- 07_projects/04_gusse_the_number/script.js
+++ 07_projects/04_gusse_the_number/script.js
@@ -1,9 +1,40 @@
 const randomNumber = parseInt(Math.random() * 100 + 1)
 
 const submit = document.querySelector("#subt")
-const userInput = document.querySelector("#inputField")
-const guessSlot = document.querySelector("#guesses")
-const lastResult = document.querySelector("#lastResult")
-const lowOrHi = document.querySelector("#lowOrHi")
+const userInput = document.querySelector("#guessField")
+// const guessSlot = document.querySelector(".guesses")
+// const lastResult = document.querySelector(".lastResult")
+// const lowOrHi = document.querySelector(".lowOrHi")
+// const startOver = document.querySelector(".resultPara")
 
+const p = document.createElement('p')
 
+let prevGuess = []
+let numGuess = 1;
+
+let playGame = true
+
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault()
+        const guess = parseInt(userInput.value)
+        console.log(guess)
+        validateGuess(guess)
+    })
+}
+
+function validateGuess(guess){
+
+}
+
+function checkGuess(guess){
+
+}
+
+function displayGuess(guess){
+
+}
+
+function dusplayMessage(message){
+
+}
