510a60652665bba60c3b7282666677b19ebe534a
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2024-01-29 07:19:07 +0000

    Dom selector

--- 06_DOM/one.js
+++ 06_DOM/one.js
@@ -80,3 +80,35 @@ NodeList(4) [li.list-item, li.list-item, li.list-item, li.list-item]
 tempLiList.forEach((li) => {
     li.style.color = 'pink';
 })
+
+
+document.getElementsByClassName('list-item')
+// HTMLCollection(4) [li.list-item, li.list-item, li.list-item, li.list-item]
+
+const tempClassList = document.getElementsByClassName('list-item')
+undefined
+tempClassList.forEach((item) => {
+    console.log(item)
+})
+// VM23012:1 Uncaught TypeError: tempClassList.forEach is not a function
+    // at <anonymous>:1:15
+
+
+
+const convertedArray = Array.from(tempClassList);
+undefined
+convertedArray
+// (4) [li.list-item, li.list-item, li.list-item, li.list-item]0: li.list-item1: li.list-item2: li.list-item3: li.list-itemlength: 4[[Prototype]]: Array(0)at: ƒ at()concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()findLast: ƒ findLast()findLastIndex: ƒ findLastIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toReversed: ƒ toReversed()toSorted: ƒ toSorted()toSpliced: ƒ toSpliced()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()with: ƒ with()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Object
+
+convertedArray.forEach((li) => {
+    console.log(li);
+})
+// VM24316:2 <li class=​"list-item" style=​"color:​ pink;​ --darkreader-inline-color:​ #ffa0b1;​" data-darkreader-inline-color>​…​</li>​
+// VM24316:2 <li class=​"list-item" style=​"color:​ pink;​ --darkreader-inline-color:​ #ffa0b1;​" data-darkreader-inline-color>​…​</li>​
+// VM24316:2 <li class=​"list-item" style=​"color:​ pink;​ --darkreader-inline-color:​ #ffa0b1;​" data-darkreader-inline-color>​…​</li>​
+// VM24316:2 <li class=​"list-item" style=​"color:​ pink;​ --darkreader-inline-color:​ #ffa0b1;​" data-darkreader-inline-color>​…​</li>​
+// undefined
+
+convertedArray.forEach((li) => {
+    li.style.backgroundColor = 'blue';
+})
